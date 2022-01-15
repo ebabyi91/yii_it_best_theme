a3bf9656998530489f092a1782d48ff996d548a5
ebabyi91 <dr8513@mail.ru>
2022-01-16 02:45:43 +0300

    #1 - first commit

---
+++ modules/admin/views/mailsfile/_search.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\MailsfileSearch */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="mailsfile-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'name') ?>
+
+    <?= $form->field($model, 'phone') ?>
+
+    <?= $form->field($model, 'file') ?>
+
+    <?= $form->field($model, 'data') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
